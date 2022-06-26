ac4ae9dd3169da96d004166cfb5e848ccc223537
Naifoudine <nmoualim@gmail.com>
2022-06-27 02:27:42 +0400

    V.0.1

---
+++ Calculate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace mauiCalculator
+{
+	public static class Calculate
+	{
+		public static double Calculer(double val1, double val2, string operatorMath)
+		{
+			double result = 0;
+
+            switch (operatorMath)
+            {
+				case "/":
+					result = val1 / val2;
+					break;
+                case "-":
+                    result = val1 - val2;
+                    break;
+                case "*":
+                    result = val1 * val2;
+                    break;
+                case "+":
+                    result = val1 + val2;
+                    break;
+                default:
+					break;
+            }
+
+            return result;
+		}
+	}
+}
+
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,24 +2,108 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	int currentState = 1;
+    string operatorMath;
+    double firstNum, secondNum;
 
-	public MainPage()
+    public MainPage()
 	{
 		InitializeComponent();
+        OnClear(this, null);
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+	void OnClear(object sender, EventArgs e)
+    {
+        firstNum = 0;
+        secondNum = 0;
+        currentState = 1;
+        this.result.Text = "0";
+    }
+    void OnClearError(object sender, EventArgs e)
+    {
+        secondNum = 0;
+        currentState = 1;
+        this.result.Text = "0";
+        Console.WriteLine("first : "+firstNum);
+    }
+    void OnPercent(object sender, EventArgs e)
+    {
+        if (firstNum == 0)
+            return;
+        firstNum = firstNum / 100;
+        this.result.Text = firstNum.ToString();
+    }
+    void OnNegative(object sender, EventArgs e)
+    {
+        if (firstNum == 0)
+            return;
+        firstNum = firstNum * -1;
+        this.result.Text = firstNum.ToString();
+    }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+    void OnNumberSelection(object sender, EventArgs e)
+    {
+        Button button = (Button)sender;
+        string btnPressed = button.Text;
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        if (this.result.Text == "0" || currentState < 0)
+        {
+            this.result.Text = string.Empty;
+            if (currentState < 0)
+                currentState *= -1;
+        }
+
+        this.result.Text += btnPressed;
+
+        double number;
+        if (double.TryParse(this.result.Text, out number))
+        {
+            //this.result.Text = number.ToString("NO");
+            if (currentState == 1)
+            {
+                firstNum = number;
+            }
+            else
+            {
+                secondNum = number;
+            }
+        }
+
+    }
+
+    void onOperatorSelection(object sender, EventArgs e)
+    {
+        currentState = -2;
+        Button button = (Button)sender;
+        string btnPressed = button.Text;
+        operatorMath = btnPressed;
+    }
+
+    void onCalculate(object sender, EventArgs e)
+    {
+        if(currentState == 2)
+        {
+            var result = Calculate.Calculer(firstNum, secondNum, operatorMath);
+            this.result.Text = result.ToString();
+            firstNum = result;
+            currentState = -1;
+
+        }
+    }
+
+
+
+    //private void OnCounterClicked(object sender, EventArgs e)
+    //{
+    //	count++;
+
+    //	if (count == 1)
+    //		CounterBtn.Text = $"Clicked {count} time";
+    //	else
+    //		CounterBtn.Text = $"Clicked {count} times";
+
+    //	SemanticScreenReader.Announce(CounterBtn.Text);
+    //}
 }
 
 
